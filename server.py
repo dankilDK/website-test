3db973b4372e8239f9a6c5bb9642f57bb7469601
dankilDK <dankilDK@yandex.ru>
2023-03-13 17:26:07 +0500

    second commit

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -9,6 +9,10 @@ app = Flask(__name__)
 def index():
     return "I'm hungry"
 
+@app.route('/tasty')
+def image():
+    return f'''<h1>Look how tasty it is!</h1><img src="{url_for('static', filename='img/tasty.jpg')}" 
+           alt="здесь должна была быть картинка, но не нашлась">'''
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
